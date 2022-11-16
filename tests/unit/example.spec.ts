25e4a1f83c0ea0b586f627b126b21c121e8abbfb
yashweblife <yashweblife20@gmail.com>
2022-11-16 04:59:48 -0600

    added another test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,9 +1,16 @@
 import { mount } from '@vue/test-utils'
 import Tab1Page from '@/views/Tab1Page.vue'
-
+import Card from '@/components/Card.vue'
 describe('Tab1Page.vue', () => {
   it('renders tab 1 Tab1Page', () => {
     const wrapper = mount(Tab1Page)
     expect(wrapper.text()).toMatch('Tab 1 page')
+  })
+})
+
+describe('Card.vue',()=>{
+  it('renders a card', ()=>{
+    const wrapper = mount(Card)
+    expect(wrapper.text()).toMatch('')
   })
 })
