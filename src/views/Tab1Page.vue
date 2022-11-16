977cf6125db24f1fb70ba48c3fbf4f9130cd1281
yashweblife <yashweblife20@gmail.com>
2022-11-16 04:59:33 -0600

    added test view

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -1,29 +1,50 @@
 <template>
   <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
-      </ion-toolbar>
-    </ion-header>
+    <HeaderComponent />
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+      <ion-list>
+        <Card v-for="item in list" :key="item.id" :title="item.title"  :image="item.url"/>
+      </ion-list>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
+import HeaderComponent from '@/components/Header.vue';
+import { IonContent, IonPage, IonList } from '@ionic/vue';
 import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
-
-export default  defineComponent({
+import Card from '@/components/Card.vue'
+export default defineComponent({
   name: 'Tab1Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { HeaderComponent, IonContent, IonPage, IonList, Card },
+  data() {
+    const list:any = []
+    return (
+      {
+        list
+      }
+    )
+  },
+  methods: {
+    getData(val:string) {
+      fetch(`https://jsonplaceholder.typicode.com/photos/${val}`)
+      .then((res: Response) => res.json())
+      .then((res) => {
+        this.list.push(res)
+      })
+    }
+  },
+  created() {
+    for(var i=0;i<10;i++){
+      this.getData(""+i)
+    }
+  }
 });
 </script>
+
+<style lang="scss" scoped>
+ion-list{
+  display:grid;
+  grid-template-columns: 1fr 1fr 1fr;
+}
+</style>
