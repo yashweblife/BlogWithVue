2bdf34c3690a83fd29ba9a53b522d440d28e89c1
yashweblife <yashweblife20@gmail.com>
2022-11-15 22:52:46 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'vue.blog.example',
+  appName: 'vue-blog-example',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
